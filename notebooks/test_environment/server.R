5869e71de6a38e0e41a5adcb4f1bd8abd20df653
Conrad <isletcore@Administrators-iMac.local>
2022-05-26 15:45:20 -0500

    worked on intensity graph

--- notebooks/test_environment/server.R
+++ notebooks/test_environment/server.R
@@ -1,29 +1,36 @@
 library(shiny)
 
 
-options(shiny.maxRequestSize=500*1024^2) 
+options(shiny.maxRequestSize=500*1024^2)
 
 # Define server logic required to draw a histogram
 shinyServer(function(input, output, session) {
+  
   
   uploaded_file <- reactive({
     
     withProgress(message = 'Converting Data',style = 'notification', value = 0, {
       req(input$file)
       
-      df <- read.csv(input$file$datapath, check.names = T)})
+      df <- read_csv(input$file$datapath)})
   })
   
   classification_col_filter <- reactive({
-    classification_cols <- uploaded_file() %>% 
-      select(Object.Id,contains('Positive.Classification'))%>%
-      rename_with(~ gsub(".Positive.Classification", "", .x, fixed = T))
+    uploaded_file() %>% 
+      select(`Object Id`,contains('Positive Classification'))%>%
+      rename_with(~ gsub(" Positive Classification", "", .x, fixed = T))
+  })
+  intensity_col_filter <- reactive({
+    uploaded_file %>% 
+      select(`Object Id`, contains('.Intensity')) %>% 
+      rename_with(~ gsub(".Intensity", "", .x, fixed = T))
+      
   })
   
   observeEvent(input$file, {
     
     marker_names <- classification_col_filter() %>%
-      select(!Object.Id) %>%
+      select(!`Object Id`) %>%
       colnames()
     
     markers = c()
@@ -41,59 +48,205 @@ shinyServer(function(input, output, session) {
     updateSelectInput(session,
                       'barChart_input',
                       choices = markers_dict)
+    updateSelectInput(session,
+                      'y_input',
+                      choices = markers_dict)
+    updateSelectInput(session,
+                      'x_input',
+                      choices = markers_dict)
     
   })
-  output$barChart <- renderPlot({
+  
+  output$intensityChart <- renderPlot({
+    
+    dataset <- uploaded_file()
+    
+    intensity_class_x <- dataset %>% 
+      select(`Object Id`, matches(input$x_input))
+    intensity_class_y <- dataset %>% 
+      select(`Object Id`, matches(input$y_input))
+    intensity_all <- intensity_class_x %>% 
+      merge(intensity_class_y)
+    class_sums <- intensity_all %>% 
+      select(Object.Id, contains('Positive Classification')) %>% 
+      mutate(sums = rowSums(.[2:3])) %>% 
+      select(`Object Id`, sums)
+    
+    intensity_with_sums <- intensity_all %>% 
+      select(Object.Id, contains('Intensity')) %>% 
+      merge(class_sums) %>% 
+      rename_with(~ gsub(" Intensity", "", .x, fixed = T)) %>% 
+      select(sums, GCG.Cell, CPEP.Cell) %>% 
+      mutate_at(vars(2:3), log)
+    
+  })
+  
+  output$cellMap <-renderPlot({
+    
+    uploaded_file() %>% 
+      select(XMin:YMax) %>% 
+      mutate(x = round((XMin + XMax)/2, 0),
+             y = round((YMin + YMax)/2, 0)) %>% 
+      select(x, y) %>% 
+      ggplot(aes(x = x, y = y))+
+      geom_point()
+    
+    
+    # fig <- plot_ly(data = x_y_coord,
+    #                x = ~x,
+    #                y = ~y)
+  })
+  
+  total_cells <- reactive({
+    as.numeric((nrow(classification_col_filter())))
+  })
+  
+  totals_barCharts <- reactive({
+    classification_col_filter() %>% 
+      summarise_at(2:ncol(classification_cols), sum) %>% 
+      select(matches(input$barChart_input))
+  })
+  totals_doughnut <- reactive({
+    classification_col_filter() %>% 
+      summarise_at(2:ncol(classification_cols), sum) %>% 
+      select(matches(input$barChart_input)) %>% 
+      mutate(total_of_subset = rowSums(across(1:last_col()))) %>% 
+      relocate(total_of_subset, .before = 1)
+  })
+  
+  output$barChart <- renderPlotly({
     
     classification_cols <- classification_col_filter()
     
     total_cells <- as.numeric(nrow(classification_cols))
     
+    
     totals <- classification_cols %>% 
-      summarise_at(2:ncol(classification_cols), sum)
-    
-    percentages <- totals %>% 
-      pivot_longer(cols = 1:ncol(totals), names_to = "markers", values_to = "total_positive") %>% 
-      mutate(percentage_of_total = round((total_positive/total_cells)*100, 2)) %>% 
-      select(markers, percentage_of_total) %>% 
-      pivot_wider(names_from = markers, values_from = percentage_of_total)
+      summarise_at(2:ncol(classification_cols), sum) %>% 
+      select(matches(input$barChart_input))
     
     
-    test3 <- classification_cols %>%
+    added_constant <- classification_cols %>%
       mutate(ID = row_number()) %>%
       pivot_longer(-ID) %>%
       filter(value != 0)
     
-    double_pos_occurances <- merge(test3, test3, by = 'ID', all = T) %>% 
+    double_pos_occurances <- merge(added_constant, added_constant, by = 'ID', all = T) %>% 
       filter(name.x != name.y) %>% 
       group_by(name.x, name.y) %>% 
       summarise(val=n()) %>% 
       pivot_wider(names_from = name.y, values_from = val, values_fill = 0, names_sort = F) %>% 
       rename(markers = name.x)
     
-    test <- double_pos_occurances %>% 
-      select(!!as.name(input$barChart_input)) %>% 
+    totals_2 <- totals %>% 
+      pivot_longer(1:ncol(totals), names_to = 'markers', values_to = 'non_dp')
+    
+    filtered_markers <- double_pos_occurances %>% 
+      select(matches(input$barChart_input)) %>% 
       ungroup()%>% 
       filter(markers %in% input$barChart_input) %>% 
       mutate(sums = rowSums(across(where(is.numeric))))
     
-    totals_2 <- totals %>% 
-      pivot_longer(1:ncol(totals), names_to = 'markers', values_to = 'non_dp')
+    # final_df <- filtered_markers %>% 
+    #     left_join(totals_2) %>% 
+    #     mutate(non_dp = non_dp - sums) %>% 
+    #     select(!sums)
     
-    test_b <- test %>% 
-      left_join(totals_2) %>% 
-      mutate(non_dp = non_dp - sums) %>% 
-      select(!sums) 
+    if (input$radio_bar == '% of whole') {
+      final_df <- filtered_markers %>% 
+        left_join(totals_2) %>% 
+        mutate(non_dp = non_dp - sums) %>% 
+        select(!sums)%>% 
+        pivot_longer(2:last_col(), names_to = 'dp_markers', values_to = 'dp_rates') %>%
+        mutate(percentage_of_total = round((dp_rates/total_cells)*100, 2)) %>% 
+        select(markers, dp_markers, percentage_of_total) %>%
+        arrange(percentage_of_total) 
+      
+      ggplotly(final_df %>% 
+                 ggplot(aes(x=markers, y = percentage_of_total, fill = reorder(dp_markers, percentage_of_total)))+
+                 geom_col())
+    }
+    else {
+      total_of_subset_prep <- totals %>% 
+        pivot_longer(1:last_col(), names_to = 'markers', values_to = 'totals_cell')
+      
+      total_of_subset <- as.numeric(sum(total_of_subset_prep$totals_cell))
+      
+      final_df <- filtered_markers %>% 
+        left_join(totals_2) %>% 
+        mutate(non_dp = non_dp - sums) %>% 
+        select(!sums) %>% 
+        mutate_if(is.numeric, ~ round(.x/total_of_subset*100,2))%>% 
+        pivot_longer(2:last_col(), names_to = 'dp_markers', values_to = 'dp_rates') %>% 
+        select(markers, dp_markers, dp_rates)
+      
+      ggplotly(final_df%>%
+                 ggplot(aes(x=markers, y = dp_rates, fill = reorder(dp_markers, dp_rates)))+
+                 geom_col())
+    }
     
-    test_b %>% 
-      pivot_longer(2:ncol(test_b), names_to = 'dp_markers', values_to = 'dp_rates') %>%
-      mutate(percentage_of_total = round((dp_rates/total_cells)*100, 2)) %>% 
-      select(markers, dp_markers, percentage_of_total) %>%
-      arrange(percentage_of_total) %>% 
-      ggplot(aes(x=markers, y = percentage_of_total, fill = reorder(dp_markers, percentage_of_total)))+
-      geom_col()
     
+  })
+  doughnut_totals <- reactive({
+    total_cells <- as.numeric(nrow(classification_col_filter()))
     
+    classification_col_filter() %>% 
+      summarise_at(2:ncol(classification_col_filter()), sum) %>% 
+      select(matches(input$barChart_input)) %>% 
+      mutate(total_of_subset = rowSums(across(1:last_col()))) %>% 
+      mutate(other_cells = total_cells - total_of_subset) %>% 
+      relocate(c(total_of_subset, other_cells), .before = 1)
+  })
+  
+  output$doughnutChart <- renderPlot({
+    
+    totals <- doughnut_totals()
+    
+    if (input$radio_donut == '% of subset') {
+      
+      subset_totals <- totals %>% 
+        pivot_longer(3:last_col(), names_to = 'selected_markers', values_to = 'total_marker_subset') %>% 
+        mutate(percent_of_subset = round((total_marker_subset/total_of_subset),3)) %>% 
+        mutate(ymax = cumsum(percent_of_subset)) %>% 
+        mutate(ymin = c(0, head(ymax, n=-1))) %>% 
+        mutate(label = paste0(selected_markers, ' \n', (percent_of_subset*100), "%"),
+               labelPosition = (ymax + ymin)/2) %>% 
+        ggplot(aes(ymax=ymax, ymin=ymin, xmax = 4, xmin=3, fill=selected_markers))+
+        geom_rect()+
+        geom_text(x=3.5, aes(y=labelPosition, label = label), size=5)+
+        geom_text(x = 2, aes(y=0, label = '% of subset'), size = 6)+
+        scale_fill_brewer(palette = 4)+
+        scale_color_brewer(palette=3) +
+        coord_polar(theta="y")+
+        xlim(c(2, 4))+
+        theme_void() +
+        theme(legend.position = "none")
+      
+      subset_totals
+    }
+    else {
+      total_cells <- as.numeric(nrow(classification_col_filter()))
+      
+      subset_of_all <- totals %>% 
+        pivot_longer(2:last_col(), names_to = 'selected_markers', values_to = 'total_marker_subset') %>% 
+        mutate(percent_of_all = round((total_marker_subset/total_cells),3)) %>%  
+        mutate(ymax = cumsum(percent_of_all)) %>% 
+        mutate(ymin = c(0, head(ymax, n=-1))) %>% 
+        mutate(label = paste0(selected_markers, ' \n', (percent_of_all*100), "%"),
+               labelPosition = (ymax + ymin)/2) %>% 
+        ggplot(aes(ymax=ymax, ymin=ymin, xmax = 4, xmin=3, fill=selected_markers))+
+        geom_rect()+
+        geom_text(x=2.80, aes(y=labelPosition, label = label), size=4)+
+        geom_text(x = 2, aes(y=0, label = '% of all'), size = 6)+
+        scale_fill_brewer(palette = 4)+
+        scale_color_brewer(palette=3) +
+        coord_polar(theta="y")+
+        xlim(c(2, 4))+
+        theme_void() +
+        theme(legend.position = "none")
+      
+      subset_of_all
+    }
   })
   
 })
--- notebooks/test_environment/ui.R
+++ notebooks/test_environment/ui.R
@@ -10,19 +10,41 @@ shinyUI(fluidPage(
       fileInput('file',
                 label = h3('File input'),
                 multiple = F,
-                accept = c("text/csv",
-                           "text/comma-separated-values,text/plain",
+                accept = c(".tsv",
                            ".csv")),
       selectInput('barChart_input',
                   'markers',
                   choices = 'none',
                   multiple = T,
-                  selectize = T)
+                  selectize = T),
+      radioButtons('radio_donut', 
+                   'Select Doghnut Chart',
+                   choices = c("% of subset",
+                               "% of whole")),
+      radioButtons('radio_bar',
+                   'Select bar chart',
+                   choices = c("% of subset",
+                               "% of whole" )),
+      selectInput('y_input',
+                  'y_axis',
+                  choices = 'none',
+                  multiple = F,
+                  selectize = T),
+      selectInput('x_input',
+                  'x_axis',
+                  choices = 'none',
+                  multiple = F,
+                  selectize = T),
+      radioButtons('log_trans',
+                   'Log Transformation',
+                   choices = c('yes', 
+                               'no'))
     ),
     
     # Show a plot of the generated distribution
     mainPanel(
-      plotOutput("barChart")
+      plotlyOutput("barChart"),
+      plotOutput("doughnutChart")
     )
   )
 ))
